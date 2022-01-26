ed03851275a15cab3a883033d5b31fb8a4f35c5a
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-26 00:07:13 -0500

    req body validation complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    test route
+// @route   POST api/users
+// @desc    register user
 // @access  public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please provide a valid email').isEmail(),
+    check('password', 'Password must be longer than 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
