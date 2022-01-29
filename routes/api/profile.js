3b8f407cd83e9a1f4325cd2d709c0c9343d21cf0
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-28 21:24:40 -0500

    profile delete route complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,22 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route   Delete api/profile
+// @desc    delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user's posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
