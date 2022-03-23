f120816cc0d6d732197a77cf34cdabf2fb2fba63
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-03-23 10:10:37 -0400

    Resolved inability to load undefined github repos in profile

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -11,29 +11,37 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   return (
     <div className='profile-github'>
       <h2 className='text-primary my-1'>Github Repos</h2>
-      {repos.map((repo) => (
-        <div key={repo.id} className='repo bg-white p-1 my-1'>
-          <div>
-            <h4>
-              <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
-                {repo.name}
-              </a>
-            </h4>
-            <p>{repo.description}</p>
+      {Array.isArray(repos) ? (
+        repos.map((repo) => (
+          <div key={repo.id} className='repo bg-white p-1 my-1'>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className='badge badge-primary'>
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className='badge badge-dark'>
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <li className='badge badge-primary'>
-                Stars: {repo.stargazers_count}
-              </li>
-              <li className='badge badge-dark'>
-                Watchers: {repo.watchers_count}
-              </li>
-              <li className='badge badge-light'>Forks: {repo.forks_count}</li>
-            </ul>
-          </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <h1>No Repos Available, Please Check Username. </h1>
+      )}
     </div>
   );
 };
