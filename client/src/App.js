605d7ba9bf48451005eeff3c6d9dd9b7a846b7ff
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-03-22 22:03:54 -0400

    Profile component complete

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import ProfileForm from './components/profile-forms/ProfileForm';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 
@@ -39,6 +40,7 @@ const App = () => {
           <Route exact path='/register' element={<Register />} />
           <Route exact path='/login' element={<Login />} />
           <Route exact path='/profiles' element={<Profiles />} />
+          <Route exact path='/profile/:id' element={<Profile />} />
           <Route
             path='/dashboard'
             element={<PrivateRoute component={Dashboard} />}
